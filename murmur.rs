f049471d8f065666115ae44b28ba75ab9a356f6b
Brian Madden <brian.a.madden@gmail.com>
2014-02-17 09:30:32 -0800

    Working murmur hash implementation

---
+++ murmur.rs
@@ -0,0 +1,110 @@
+use std::iter::range_step;
+use std::io::BufferedReader;
+use std::io::stdin;
+
+fn murmur3_32(key: &str, seed: u32) -> u32 {
+    
+    let c1 = 0xcc9e2d51;
+    let c2 = 0x1b873593;
+    let r1 : u32 = 15;
+    let r2 : u32 = 13;
+    let m : u32 = 5;
+    let n = 0xe6546b64;
+
+    let mut hash = seed;
+    let key_bytes: &[u8] = key.as_bytes();
+    let len = key_bytes.len();
+
+    for byte_index in range_step(0, len, 4) {
+
+        // Check against len -1 since we index from 0
+        if (byte_index + 3) <= (len - 1) {
+
+            // Slice is from [x, y) so we'll use byte_index, byte_index +4
+            let mut chunk = key_bytes_to_u32_chunk(
+                key_bytes.slice(byte_index, byte_index+4));
+                
+            chunk = chunk * c1;
+            chunk = (chunk << r1) | (chunk >> (32 - r1));
+            chunk = chunk * c2;
+            
+            hash = hash ^ chunk;
+            hash = (hash << r2) | (hash >> (32 - r2));
+            hash = (hash * m) + n;
+            
+        } else {
+            // If we have less than four...
+            // Make sure to slice to len + 1 to cover the final byte
+            let mut chunk = key_bytes_to_u32_chunk(
+                key_bytes.slice(byte_index, len));
+
+
+            chunk = chunk * c1;
+            chunk = (chunk << r1) | (chunk >> (32 - r1));
+            chunk = chunk * c2;
+
+            hash = hash ^ chunk;
+        }
+    }
+
+    hash = hash ^ (len as u32);
+    hash = hash ^ (hash >> 16);
+    hash = hash * 0x85ebca6b;
+    hash = hash ^ (hash >> 13);
+    hash = hash * 0xc2b2ae35;
+    hash = hash ^ (hash >> 16);
+
+    return hash;
+
+}
+
+
+fn key_bytes_to_u32_chunk(bytes: &[u8]) -> u32 {
+    // TODO: Ensure that we're dealing with LE architecture, 
+    // if not flip the bytes
+
+    let foo: u32 = match bytes.len() {
+        
+        4 => {
+            ((bytes[3] as u32 << 24) + 
+             (bytes[2] as u32 << 16) + 
+             (bytes[1] as u32 << 8) + 
+             (bytes[0] as u32)) as u32
+        },
+            
+        3 => { 
+            ((bytes[2] as u32 << 16) + 
+             (bytes[1] as u32 << 8) + 
+             (bytes[0] as u32)) as u32
+        },
+        
+        2 => {
+            ((bytes[1] as u32 << 8) + 
+             (bytes[0] as u32)) as u32
+        },
+
+        1 => { bytes[0] as u32 },
+        
+        _ => 0
+            
+    };
+    
+    println!("foo = {}", foo);
+    return foo;
+ 
+}
+
+fn main() {
+
+    let mut stdin = BufferedReader::new(stdin());
+    for line in stdin.lines() {
+        let chomped = line.slice(0, line.len() - 1);
+ 
+        if chomped == "exit" { break; }
+        
+        let foo = murmur3_32(chomped, 0);
+        println!("FINAL HASH = {}", foo);
+
+    }
+    
+}
